6ff5ccf5fed3a09f126054a4d03df517af4c7342
Sina Rostam <Sinarostam@icloud.com>
2015-02-11 15:47:45 -0800

    images update

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAJOBJ3RWWAZ3SSKGA',                        # required
-    :aws_secret_access_key  => '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',                               # required
+    :aws_access_key_id      => 'AKIAIQ4MCQICQNTJHOGA',                        # required
+    :aws_secret_access_key  => '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',                               # required
 }
-  config.fog_directory  = 'yelpdemosina'                          # required
+  config.fog_directory  = 'sinatest1'                    # required
 end
