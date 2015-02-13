9bad28cb2dc0baafe2e3370b03549015b7f0dbd2
Sina Rostam <Sinarostam@icloud.com>
2015-02-12 22:29:15 -0800

    add custom css

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,10 +4,15 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -23,7 +28,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  process :resize_to_fit => [200, 300]
   #
   # def scale(width, height)
   #   # do something
